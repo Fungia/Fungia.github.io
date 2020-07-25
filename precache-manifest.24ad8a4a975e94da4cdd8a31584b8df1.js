4c09f20cc3a42732bc10f65224e58b977a13d81c
Fungia <68678324+Fungia@users.noreply.github.com>
2020-07-25 10:06:13 +0200

    Updates

---
+++ precache-manifest.24ad8a4a975e94da4cdd8a31584b8df1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28c2b68e85f898a982d2654d3cb0bd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1501bf363a5b381aca34",
+    "url": "/static/css/main.0c59054c.chunk.css"
+  },
+  {
+    "revision": "5c57b0b294c53969f51a",
+    "url": "/static/js/2.8eeb9bb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8eeb9bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1501bf363a5b381aca34",
+    "url": "/static/js/main.39928358.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "9e9088e3d6048071315147335b90751d",
+    "url": "/static/media/fotka.9e9088e3.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
